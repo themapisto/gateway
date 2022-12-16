b8469740291cbe2262c713828d762df080634342
themapisto <themapisto@naver.com>
2022-12-16 16:10:31 +0900

    re

---
+++ src/main/java/com/example/gatway/DController.java
@@ -0,0 +1,23 @@
+package com.example.gatway;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/service1")
+@Slf4j
+public class DController {
+
+
+    @GetMapping(value = "/info")
+    public String services() {
+
+        return "Hello";
+    }
+
+
+}
---
+++ src/main/java/com/example/gatway/GatwayApplication.java
@@ -0,0 +1,15 @@
+package com.example.gatway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatwayApplication.class, args);
+	}
+
+}
