df090e54cc090ffc79969e014015003a650239a7
Advanc2d <phm940916@gmail.com>
2023-05-23 13:13:19 +0900

    1. 사용하지 않는 import 제거 2. yml 파일 수정

--- src/main/java/com/example/gatway/GatwayApplication.java
+++ src/main/java/com/example/gatway/GatwayApplication.java
@@ -4,13 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.util.Stack;
-import java.util.StringTokenizer;
-
 @SpringBootApplication
 @EnableEurekaClient
 public class GatwayApplication {
